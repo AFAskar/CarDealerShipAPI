1c96d321cd64133bc42f7c42d05727cd5a755d4c
AF_Askar <68832286+abo3skr2019@users.noreply.github.com>
2025-12-24 16:58:16 +0300

    migr

--- CarDealership.Api/Migrations/20251224135708_SecondRun.Designer.cs
+++ CarDealership.Api/Migrations/20251224135708_SecondRun.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CarDealership.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarDealership.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251224135708_SecondRun")]
+    partial class SecondRun
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
---
+++ migrate.sh
@@ -0,0 +1,2 @@
+dotnet ef migrations add SecondRun --project /home/azoz-laptop/Desktop/Git-Projects/jyadcar/CarDealership.Api/CarDealership.Api.csproj && \
+dotnet ef database update --project /home/azoz-laptop/Desktop/Git-Projects/jyadcar/CarDealership.Api/CarDealership.Api.csproj
